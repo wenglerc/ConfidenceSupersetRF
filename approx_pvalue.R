c47eee2aee27bfc7bebc42435d6cf67cc38ad85d
wenglerc <carolin.wnglr@gmail.com>
2022-09-17 19:05:36 +0200

    Fortschritt: Anpassungen in tGKF und ConfSet, Simulation mehrerer Testfälle, erste Basis für Auswertung. (TODO: ConfSet ist bisher zu übergenau! Das ist schlecht und muss korrigiert werden. parallel Package nutzen für schnellere Auswertung)

--- approx_pvalue.R
+++ approx_pvalue.R
@@ -1,4 +1,5 @@
 library(dplyr)
+library(stats)
 
 # Erstellung einer Partitionsfolge (degenerierend, verfeinernd) -----------
 
@@ -12,13 +13,13 @@ library(dplyr)
 #' @export
 #'
 #' @examples
-partition_seq <- function(S, max_iterations = 2000) {
+partition_seq <- function(S, pieces = 2, max_iterations = 2000) {
     iterations <- min(length(S), max_iterations)
     
-    result <- lapply(1:(log2(iterations) + 1), function(iter) {
+    result <- lapply(1:(log(iterations, base = pieces) + 1), function(iter) {
         split(S, cut(seq_along(S),
-                     # Intervalle halbieren
-                     breaks = 2 ^ iter,
+                     # Intervalle in pieces Teile teilen
+                     breaks = pieces ^ iter,
                      labels = FALSE))
     })
     
@@ -35,23 +36,31 @@ tGKF <- function(S, data, basisEC, level) {
     # EC Dichten p_0 und p_1
     degree = ncol(data) - 1
     p0 <- 1 - pt(level, degree)
-    p1 <- (1 + level ^ 2 / degree) ^ (-(degree - 1) / 2) / (2 * pi)
+    p1 <- (2 *pi)^(-1) * (1 + level ^ 2 / degree)^(-(degree - 1) / 2)
     
     # LKC_0
     L0 <- basisEC
     
     # LKC_1 gemaess Schaetzung
-    residuals <- (data - rowSums(data) / ncol(data) ) / apply(data, 1, sd)
+    residuals <-
+        (data - rowSums(data) / ncol(data)) / apply(data, 1, sd)
     
-    df.residuals <- # Differenzenquotient
-        ( (rbind(residuals[-1, ], residuals[length(residuals), ]) - residuals ) / diff(S)) %>%
-        t() %>% apply(2, var) %>% sqrt()
+    if (length(S) > 1) {
+        # df.residuals <- # Differenzenquotient
+        #     ((rbind(residuals[-1,], residuals[nrow(residuals),]) - residuals) / diff(S)) %>%
+        #     t() %>% apply(2, var) %>% sqrt()
+        df.residuals <- apply(residuals, 2, function(values) {
+            func <- stats::splinefun(S, values, method = "natural")
+            deriv <- func(S, deriv = 1)
+        }) %>% apply(1, sd)
+        L1 <- # Trapezregel
+            sum(diff(S) / 2 * (df.residuals[-length(df.residuals)] + df.residuals[-1]))
+    } else {
+        L1 <- 0
+    }
     
-    L1 <- # Trapezregel
-        sum(diff(S) / 2 * (df.residuals[-length(df.residuals)] + df.residuals[-1]))
-    
-    # Bestimme das Ergebnis
-    return(p0 * L0 + p1 * L1)
+    # Bestimme die EEC
+    p0 * L0 + p1 * L1
 }
 
 
@@ -111,22 +120,78 @@ ConfSet <- function(data,
                 partS[sortorder[i:N]] %>% unlist() %>% unname() %>% sort()
             })
         
-        # Berechnung der p-Werte per Expected Euler Charakteristic
+        # Berechnung der p-Werte
+        accept.index = 0
+        for (i in 1:N) {
+            basisEC <- 1    # EC von S = [0,1]
+            pvalue <- tGKF(sortedUnions[[i]],
+                           data[S %in% sortedUnions[[i]],],
+                           basisEC,
+                           teststatistics[i]) %>% unname()
+            # Akzeptiere V_k, wenn p(x_k, V_k) >= alpha
+            if (pvalue >= alpha){
+                accept.index <- i
+                break
+            }
+        }
+        
+        # Erweitere U_n = U_{n-1} + V_k
+        if (accept.index > 0) U <- sort(union(U, sortedUnions[[accept.index]]))
+
+    }
+    
+    return(U)
+}
+
+ConfSet2 <- function(data,
+                    S,
+                    partitions,
+                    alpha,
+                    level){
+    
+    # Initialisierungen ----
+    samplesize <- ncol(data)
+    t.statistic <- - (rowSums(data) - level) / sqrt(samplesize) / apply(data, 1, sd)
+    
+    U <- c()
+    
+    # Aussere Schleife: Loop über die Folge an Partitionen ----
+    for (partS in partitions) {
+        # Reduktion der aktuell betrachteten Partition auf jene Teilmengen,
+        # die zuvor noch nicht zu U gezaehlt wurden: partS^*_n = partS_n \ U_{n-1}
+        partS <- lapply(partS, function(vec) if (!all(vec %in% U)) vec) %>%
+            Filter(Negate(is.null), .)
+        if (all(sapply(partS, is.null))) break
+        
+        N <- length(partS)
+        
+        # Innere Schleife: Berechnung von U_n nach Partition partS^*_n ----
+        
+        # Berechnung und Sortierung der realisierten Werte der Teststatistik
+        teststatistics <- lapply(partS, function(vec) t.statistic[S %in% vec] %>% max()) %>%
+            unlist()
+        sortorder <- order(teststatistics, decreasing = T)
+        teststatistics <- sort(teststatistics, decreasing = T)
+        
+        # Vereinigungen V_k ab dem k-ten Partitionselement (geordnet wie Teststatisik)
+        sortedUnions <-
+            lapply(1:N, function(i) {
+                partS[sortorder[i:N]] %>% unlist() %>% unname() %>% sort()
+            })
+        
+        # Berechnung der p-Werte
         basisEC <- 1    # EC von S = [0,1]
-        pvalues <- sapply(1:N, function(i) {
+        pvalues <- sapply(1:N, function(i){
             tGKF(sortedUnions[[i]],
                  data[S %in% sortedUnions[[i]],],
                  basisEC,
                  teststatistics[i])
         })
-        
+
         # Akzeptiere V_k, wenn p(x_k, V_k) >= alpha
-        tempU <- c()
-        k <- min(which(pvalues >= alpha))
-        if (k <= N) tempU <- sortedUnions[[k]]
-        
-        # tempU zu U hinzufuegen
-        U <- union(U, tempU)
+        if (any(pvalues >= alpha)) {
+            U <- sort(union(U, sortedUnions[[min(which(pvalues >= alpha))]]))
+        }
     }
     
     return(U)
--- example.R
+++ example.R
@@ -3,6 +3,7 @@ library(mvtnorm)
 library(tidyverse)
 library(reshape2)
 library(ggplot2)
+library(microbenchmark)
 
 theme_set(theme_light())
 
@@ -30,13 +31,14 @@ linearModell <- function(n, S, mu, eps) {
 #### Initialisierung globaler Variablen -----------------------------------
 
 randomseed <- 1
-samplesize <- 50
-gridwidth <- 1e-3               # Schrittweite der Diskretisierung der Grundmenge
+samplesize.max <- 200
+iterations <- 1000
+gridpoints <- 200               # Anzahl equidistanter Gitterpunkte
 
 alpha <- 0.05
 level <- 0                      # Grenzwert aus der Null-Hypothese
-S <- seq(0, 1, by = gridwidth)  # Grundmenge S = [0,1]
-partitions <- partition_seq(S)  # Folge von Partitionen (degenerierend, feiner werdend)
+S <- seq(0, 1, length.out = gridpoints)  # Grundmenge S = [0,1]
+partitions <- partition_seq(S, pieces = 2)  # Folge von Partitionen (degenerierend, feiner werdend)
 
 
 #### Testfaelle generieren --------------------------------------------------
@@ -44,18 +46,45 @@ partitions <- partition_seq(S)  # Folge von Partitionen (degenerierend, feiner w
 set.seed(randomseed)
 
 # Realisierungen von X
-data <- data.frame(linearModell(samplesize, S, mu, eps))
+data.all <- lapply(1:iterations, function(i) {
+    data.frame(linearModell(samplesize.max, S, mu, eps))
+})
 data.mu <- data.frame(S = S, Erwartungswert = mu(S))
-data.standardized <- data.frame(
-    S = S,
-    Mittelwert.normiert = rowSums(data[,-1]) / sqrt(samplesize) / apply(data[,-1], 1, sd)
-    )
 
-# Darstellung einiger Realisierungen
-data.plot <- melt(data.frame(S=S, data)[, 1:10], id.vars = "S", variable.name = 'Sample', value.name = "Wert")
+# Darstellung von Realisierungen
+data.plot <- melt(
+    data.frame(S = S, data.all[[1]])[, 1:10],
+    id.vars = "S",
+    variable.name = 'Sample',
+    value.name = "Wert"
+)
 p <- ggplot() +
-    labs(x ="Grundmenge S", y = "", colour = "Realisierung") +
-    theme(legend.position="none")
+    labs(x = "Grundmenge S", y = "", colour = "Realisierung") +
+    theme(legend.position = "none")
 p + geom_line(data = data.plot, aes(S, Wert, colour = Sample), alpha = 0.5) +
     geom_line(data = data.mu, aes(S, Erwartungswert), colour = "red")
+
+#### Konstruktion --------------------------------------------------
+
+# U <- ConfSet(data, S, partitions, alpha, level)
+# cat("Genauigkeit: ", length(U[!(U %in% S[mu(S) > level])]) / length(U))
+
+# mbm <- microbenchmark(
+#     V1 = ConfSet(data, S, partitions, alpha, level),
+#     V2 = ConfSet2(data, S, partitions, alpha, level),
+#     times = 10
+# )
+# 
+# autoplot(mbm)
+
+samplesize.list <- c(10,20,50,100,150,200)
+
+results <- sapply(samplesize.list, function(N){
+    results.U <- lapply(data.all, function(data) ConfSet(data[,1:N], S, partitions, alpha, level))
+    fullcovered <- sapply(results.U, function(U) all(S[data.mu$Erwartungswert > level] %in% U))
+    
+    covering.rate <- length(fullcovered[fullcovered == T]) / length(fullcovered)
+})
+
+
 
