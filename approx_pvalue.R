c36a09b672c5ed83b3b4acb2cacf3dac1087478e
wenglerc <carolin.wnglr@gmail.com>
2022-09-23 20:37:48 +0200

    Fortschritt: Korrektur MB und zweite Version von ConfSet mit MB außerhalb der Schleife (TODO: sd ohne apply berechnen ?)

--- approx_pvalue.R
+++ approx_pvalue.R
@@ -33,12 +33,12 @@ partition_seq <- function(S, pieces = 2, max_iterations = 2000) {
 
 # Expected Euler Characteristic via Gaussian Kinematic Formular -----------
 
-tGKF <- function(S, data, level, basisEC = 1) {
+tGKF <- function(S, data, threshold, basisEC = 1) {
     
     # EC Dichten p_0 und p_1
     degree = ncol(data) - 1
-    p0 <- 1 - pt(level, degree)
-    p1 <- (2 *pi)^(-1) * (1 + level ^ 2 / degree)^(-(degree - 1) / 2)
+    p0 <- 1 - pt(threshold, degree)
+    p1 <- (2 *pi)^(-1) * (1 + threshold ^ 2 / degree)^(-(degree - 1) / 2)
     
     # LKC_0
     L0 <- basisEC
@@ -62,14 +62,14 @@ tGKF <- function(S, data, level, basisEC = 1) {
     p0 * L0 + p1 * L1
 }
 
-mBoot <- function(S, data, level, iter = 1000) {
+mBoot <- function(set, X, threshold, iter = 1000) {
     
-    samplesize = ncol(data)
-    S.size = length(S)
+    samplesize = ncol(X)
+    S.size = length(set)
     
     # Residuen
     residuals <-
-        sqrt(samplesize / (samplesize - 1)) * (data - rowSums(data) / samplesize) %>%
+        sqrt(samplesize / (samplesize - 1)) * (X - rowSums(X) / samplesize) %>%
         as.matrix()
     
     # Standardnormalverteilte Multiplier
@@ -78,18 +78,17 @@ mBoot <- function(S, data, level, iter = 1000) {
         aperm(c(3, 1, 2))
     
     # Standardabweichung nach Anwendung der Multiplier
-    g.times.data <-
-        g * array(as.matrix(data), dim = c(dim(data), iter))
-    sd <- apply(g.times.data, c(1, 3), sd) %>%
+    g.times.res <-
+        g * array(residuals, dim = c(dim(residuals), iter))
+    sd <- apply(g.times.res, c(1, 3), sd) %>%
         array(c(dim(.), samplesize)) %>%
         aperm(c(1, 3, 2))
     
     # Maximum der Summen gewichteter Residuen über der Grundmenge
-    mboot.distr <- ( g * array(residuals, dim = c(dim(residuals), iter)) /
-                         sd / sqrt(samplesize)) %>%
+    mboot.distr <- ( g.times.res / sd / sqrt(samplesize)) %>%
         apply(3, function(mat) max(rowSums(mat)) )
     
-    length(mboot.distr[mboot.distr >= level]) / length(mboot.distr)
+    length(mboot.distr[mboot.distr >= threshold]) / length(mboot.distr)
     
 }
 
@@ -155,7 +154,7 @@ ConfSet <- function(data,
             for (i in N:1) {
                 pvalue <- tGKF(sortedUnions[[i]],
                                data[S %in% sortedUnions[[i]], ],
-                               teststatistics[i],
+                               threshold = teststatistics[i],
                                basisEC = 1) %>% unname()
                 # Akzeptiere V_k, wenn p(x_k, V_k) >= alpha
                 if (pvalue < alpha) {
@@ -165,9 +164,9 @@ ConfSet <- function(data,
             }
         } else if (pmethod == "mboot") {
             for (i in N:1) {
-                pvalue <- mBoot(sortedUnions[[i]],
-                                data[S %in% sortedUnions[[i]], ],
-                                teststatistics[i]) %>% unname()
+                pvalue <- mBoot(set = sortedUnions[[i]],
+                                X = data[S %in% sortedUnions[[i]], ],
+                                threshold = teststatistics[i]) %>% unname()
                 # Akzeptiere V_k, wenn p(x_k, V_k) >= alpha
                 if (pvalue < alpha) {
                     accept.index <- ifelse(i == N, 0, i + 1)
@@ -184,3 +183,119 @@ ConfSet <- function(data,
     sort(U)
 
 }
+
+#' Berechnung einer Konfidenz-Obermenge des Excursion Sets
+#' S_0 = \eqn{\{\mu(s) > level\}} bzgl. eines Zufallsfelds
+#' \eqn{X_s(omega) = mu(s) + eps_s(omega)} zu einer kompakten Grundmenge S mit
+#' Eulercharakteristik 1.
+#' Die Berechnung basiert auf der Metode aus
+#' "False Discovery Control for Random Fields" (2004) - M. Perone Pacifico,
+#' C. Genovese, I. Verdinelli and L. Wasserman
+#'
+#' @param df Realisierungen des Zufallsfeldes
+#' @param S Grundmenge S (1D und EC(S) = 1)
+#' @param partitions Partitionierung der Grundmenge (degenerierend und verfeinernd)
+#' @param alpha Signifikanzniveau für die Konfidenzmenge
+#' @param level Grenzwert des Excursion Sets
+#'
+#' @return Konfidenzmenge U mit \eqn{P(S_0 \subset U) \geq 1 - \alpha}
+#' @export
+#'
+#' @examples
+ConfSet2 <- function(data,
+                    S,
+                    partitions,
+                    alpha,
+                    level,
+                    pmethod = "tgkf",
+                    mb.iter = 1000){
+    
+    # Initialisierungen ----
+    samplesize <- ncol(data)
+    t.statistic <- - sqrt(samplesize) * (rowSums(data)/samplesize - level) / apply(data, 1, sd)
+    
+    if (pmethod == "mboot"){
+        
+        S.size = length(S)
+        # Residuen
+        residuals <-
+            sqrt(samplesize / (samplesize - 1)) * (data - rowSums(data) / samplesize) %>%
+            as.matrix()
+        # Standardnormalverteilte Multiplier
+        g <- rnorm(samplesize * mb.iter) %>%
+            array(c(samplesize, mb.iter, S.size)) %>%
+            aperm(c(3, 1, 2))
+        # Standardabweichung nach Anwendung der Multiplier
+        g.times.res <-
+            g * array(residuals, dim = c(dim(residuals), mb.iter))
+        sd <- apply(g.times.res , c(1, 3), sd) %>%
+            array(c(dim(.), samplesize)) %>%
+            aperm(c(1, 3, 2))
+        # Summen gewichteter Residuen über der Grundmenge
+        mboot <- ( g.times.res / sd / sqrt(samplesize)) %>%
+            apply(3, function(mat) rowSums(mat) )
+        
+        rm(residuals, g, g.times.res, sd)
+    }
+    
+    U <- rep(NA, length(S))
+    
+    # Aussere Schleife: Loop über die Folge an Partitionen ----
+    for (partS in partitions) {
+        # Reduktion der aktuell betrachteten Partition auf jene Teilmengen,
+        # die zuvor noch nicht zu U gezaehlt wurden: partS^*_n = partS_n \ U_{n-1}
+        partS <- lapply(partS, function(vec) if (!all(vec %in% U)) vec) %>%
+            Filter(Negate(is.null), .)
+        if (all(sapply(partS, is.null))) break
+        
+        N <- length(partS)
+        # Innere Schleife: Berechnung von U_n nach Partition partS^*_n ----
+        
+        # Berechnung und Sortierung der realisierten Werte der Teststatistik
+        teststatistics <- sapply(partS, function(vec) t.statistic[S %in% vec] %>% max())
+        sortorder <- order(teststatistics, decreasing = T)
+        teststatistics <- sort(teststatistics, decreasing = T)
+        
+        # Vereinigungen V_k ab dem k-ten Partitionselement (geordnet wie Teststatisik)
+        sortedUnions <-
+            lapply(1:N, function(i) {
+                partS[sortorder[i:N]] %>% unlist() %>% unname() %>% sort()
+            })
+        
+        # Berechnung der p-Werte
+        accept.index = 1
+        if (pmethod == "tgkf") {
+            for (i in N:1) {
+                pvalue <- tGKF(sortedUnions[[i]],
+                               data[S %in% sortedUnions[[i]], ],
+                               threshold = teststatistics[i],
+                               basisEC = 1) %>% unname()
+                # Akzeptiere V_k, wenn p(x_k, V_k) >= alpha
+                if (pvalue < alpha) {
+                    accept.index <- ifelse(i == N, 0, i + 1)
+                    break
+                }
+            }
+        } else if (pmethod == "mboot") {
+            for (i in N:1) {
+                mboot.max <- mboot[S %in% sortedUnions[[i]],] %>%
+                    t() %>% data.frame() %>% do.call(pmax, .)
+                pvalue <- length(mboot.max[mboot.max >= teststatistics[i]]) /
+                    length(mboot.max)
+                # Akzeptiere V_k, wenn p(x_k, V_k) >= alpha
+                if (pvalue < alpha) {
+                    accept.index <- ifelse(i == N, 0, i + 1)
+                    break
+                }
+            }
+        }
+        
+        # Erweitere U_n = U_{n-1} + V_k
+        if (accept.index > 0) U <- union(U, sortedUnions[[accept.index]])
+        
+    }
+    
+    sort(U)
+    
+}
+
--- example.R
+++ example.R
@@ -6,6 +6,8 @@ require(ggplot2)
 require(doParallel)
 require(microbenchmark)
 
+library(tictoc)
+
 theme_set(theme_light())
 
 # Generieren eines Zufallsprozesses X(s) = mu(s) + sigma(s)*epsilon(s) --------
@@ -33,13 +35,13 @@ linearModell <- function(n, S, mu, eps) {
 
 randomseed <- 1
 samplesize.max <- 100
-iterations <- 250
+iterations <- 100
 gridpoints <- 200               # Anzahl equidistanter Gitterpunkte
 
 alpha <- 0.05
 level <- 0                      # Grenzwert aus der Null-Hypothese
 S <- seq(0, 1, length.out = gridpoints)  # Grundmenge S = [0,1]
-partitions <- partition_seq(S, pieces = 4)  # Folge von Partitionen (degenerierend, feiner werdend)
+partitions <- partition_seq(S, pieces = 3)  # Folge von Partitionen (degenerierend, feiner werdend)
 
 
 #### Testfaelle generieren --------------------------------------------------
@@ -64,34 +66,38 @@ data.plot <- reshape2::melt(
     variable.name = 'Sample',
     value.name = "Wert"
 )
-p <- ggplot() +
+p.ex <- ggplot() +
     labs(x = "Grundmenge S", y = "", colour = "Realisierung") +
     theme(legend.position = "none")
-p + geom_line(data = data.plot, aes(S, Wert, colour = Sample), alpha = 0.5) +
+p.ex + geom_line(data = data.plot, aes(S, Wert, colour = Sample), alpha = 0.5) +
     geom_line(data = data.mu, aes(S, Erwartungswert), colour = "red")
 
 #### Konstruktion --------------------------------------------------
 
-U <- ConfSet(data.all[[6]], S, partitions, alpha, level, pmethod = "mboot")
-cat("S0 nicht in U: ", length( S0[ !(S0 %in% U)] ) / length(S0),
-    "\nU nicht in S0: ", length( U[ !(U %in% S0)] ) / length(U))
-
-# mbm <- microbenchmark(
-#     V1 = ConfSet(data.all[[1]], S, partitions, alpha, level),
-#     V2 = ConfSet2(data.all[[1]], S, partitions, alpha, level),
-#     times = 10
-# )
+# U <- ConfSet(data.all[[3]], S, partitions, alpha, level, pmethod = "mboot")
+# cat("S0 nicht in U: ", length( S0[ !(S0 %in% U)] ) / length(S0),
+#     "\nU nicht in S0: ", length( U[ !(U %in% S0)] ) / length(U))
 # 
-# autoplot(mbm)
+# U <- ConfSet2(data.all[[3]], S, partitions, alpha, level, pmethod = "mboot")
+# cat("S0 nicht in U: ", length( S0[ !(S0 %in% U)] ) / length(S0),
+#     "\nU nicht in S0: ", length( U[ !(U %in% S0)] ) / length(U))
 
-samplesize.list <- c(10,50,100,200,500)
+mbm <- microbenchmark(
+    V1 = ConfSet(data.all[[1]], S, partitions, alpha, level, pmethod = "mboot"),
+    V2 = ConfSet2(data.all[[1]], S, partitions, alpha, level, pmethod = "mboot"),
+    times = 5
+)
+autoplot(mbm)
 
-results <- sapply(samplesize.list, function(N) {
+samplesize.list <- c(20,50,100,200,500)
+
+tic("GKF")
+results.gkf <- sapply(samplesize.list, function(N) {
     cluster <- makeCluster(detectCores() - 1)
     registerDoParallel(cluster)
     results.U <-
         foreach(data = data.all, .export = ls(globalenv()), .packages = c("dplyr", "stats")) %dopar% {
-            ConfSet(data[, 1:N], S, partitions, alpha, level, pmethod = "mboot")
+            ConfSet(data[, 1:N], S, partitions, alpha, level, pmethod = "tgkf")
         }
     stopCluster(cluster)
 
@@ -102,5 +108,40 @@ results <- sapply(samplesize.list, function(N) {
         length(fullcovered[fullcovered == T]) / length(fullcovered)
 
 })
+toc()
+
+tic("MBoot")
+results.mb <- sapply(samplesize.list, function(N) {
+    cluster <- makeCluster(detectCores() - 1)
+    registerDoParallel(cluster)
+    results.U <-
+        foreach(data = data.all, .export = ls(globalenv()), .packages = c("dplyr", "stats")) %dopar% {
+            ConfSet(data[, 1:N], S, partitions, alpha, level, pmethod = "mboot")
+        }
+    stopCluster(cluster)
+    
+    fullcovered <-
+        sapply(results.U, function(U)
+            all(S[data.mu$Erwartungswert > level] %in% U))
+    covering.rate <-
+        length(fullcovered[fullcovered == T]) / length(fullcovered)
+    
+})
+toc()
+
+#### Auswertung --------------------------------------------------
+
+df.gkf <- data.frame(samplesize = samplesize.list,
+                     res = results.gkf, method = "t-GKF")
+# df.mb <- data.frame(samplesize = samplesize.list,
+#                     res = results.mb, method = "Multiplier Bootstrap")
+# data.plot <- rbind(df.gkf, df.mb)
+
+ggplot(data = df.gkf, aes(x = samplesize, y = res, color = method)) +
+    labs(x = "Anzahl an Samples", y = "Überdeckungsrate", color = "Methode") +
+    theme(legend.position = "top") +
+    geom_line() + geom_point() +
+    geom_hline(yintercept = 1 - alpha, linetype = 'dashed', col = 'red')
+
 
 
